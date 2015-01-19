687bbacf02b98775154007575697aba19ce88d3a
pysh <pysh@75a46143-9d88-471c-a7d8-8d193f508941>
2015-01-19 10:24:19 +0000

    Перемещение в Trunk

---
+++ ccsxml2pdf/Program.vb
@@ -0,0 +1,26 @@
+﻿'
+' Сделано в SharpDevelop.
+' Пользователь: pnosov
+' Дата: 30.01.2013
+' Время: 11:20
+' 
+' Для изменения этого шаблона используйте Сервис | Настройка | Кодирование | Правка стандартных заголовков.
+'
+Imports Microsoft.VisualBasic.ApplicationServices
+
+Namespace My
+	' This file controls the behaviour of the application.
+	Partial Class MyApplication
+		Public Sub New()
+			MyBase.New(AuthenticationMode.Windows)
+			Me.IsSingleInstance = False
+			Me.EnableVisualStyles = True
+			Me.SaveMySettingsOnExit = True
+			Me.ShutDownStyle = ShutdownMode.AfterMainFormCloses
+		End Sub
+		
+		Protected Overrides Sub OnCreateMainForm()
+			Me.MainForm = My.Forms.MainForm
+		End Sub
+	End Class
+End Namespace
